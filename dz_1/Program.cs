a3d1d8910e2ab99111593e4ee0e2eef1d1d36c82
Arteeex <arteeex187@gmail.com>
2023-01-30 13:25:29 +0700

    ready_BAZA

---
+++ dz_1/Program.cs
@@ -0,0 +1,16 @@
+﻿int Pow (int num, int n)
+{
+    int res = num;
+    for(int i = 1; i<n; i++)
+    {
+        res *= num;
+    }
+    return res;
+}
+Console.Write("Введите число: ");
+int value1 = int.Parse(Console.ReadLine());
+Console.Write("Введите степень: ");
+int value2 = int.Parse(Console.ReadLine());
+
+int res = Pow(value1, value2);
+Console.WriteLine(res);
